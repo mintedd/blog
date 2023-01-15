67075a3e7d346127b5f7ff3ddfa8ea1c657845df
mintedd <xxxxiheartyouxxxxx@gmail.com>
2023-01-14 23:37:07 -0500

    dynamically added header and footer

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const Post = require('..//models/Post');
 
+//Get all posts from db so you can render on homepage existing posts
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll({
-    });
+    const postData = await Post.findAll();
 
     const posts = postData.map((data) => data.get({ plain: true }));
 
@@ -17,10 +17,10 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const newPost = await Post.create({
-      ...req.body,
-      user_id: req.session.user_id,
+      title,
+      body,
     });
-
+    res.render(newPost)
     res.status(200).json(newPost)
   } catch (err) {
     res.status(400).json(err);
